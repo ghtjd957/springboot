3d7933c4434857c94e936ecb4135885790cfb9fd
ghtjd957 <ghtjd957@naver.com>
2021-11-08 16:26:04 +0900

    Initial commit

---
+++ src/main/java/com/hoseong/book/ex1/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,12 @@
+package com.hoseong.book.ex1.springboot.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+    private final String name;
+    private final int amount;
+
+}
